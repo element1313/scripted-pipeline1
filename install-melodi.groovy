4c2255df1c5c7fbfb24139271a2f842821b2d4cf
element1313 <xurhohs@gmail.com>
2021-04-15 05:44:10 +0000

    added docker terraform vpc

--- install-melodi.groovy
+++ install-melodi.groovy
@@ -7,13 +7,8 @@ node{
     stage("Pull repo"){
         sh "rm -rf ansible-melodi && git clone https://github.com/ikambarov/ansible-melodi.git"
     }
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-        stage("Install Melodi"){
-            sh """
-            export ANSIBLE_HOST_KEY_CHECKING=False
-            ansible-playbook -i  "${params.node}," --private-key $SSHKEY  ansible-melodi/main.yml -b -u $SSHUSERNAME
-            """
-        }
+    stage("Install Melodi"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node}",  playbook: 'ansible-melodi/main.yml', sudo: true
     }
 }
     
